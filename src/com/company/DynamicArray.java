52bff7181ada976753ba737bbad5de9c412be1ff
ashrafffathalla <75317437+ashrafffathalla@users.noreply.github.com>
2022-05-12 12:51:44 +0200

    commit and push

--- src/com/company/DynamicArray.java
+++ src/com/company/DynamicArray.java
@@ -2,5 +2,14 @@ package com.company;
 
 public class DynamicArray
 {
-
+    int[] data;
+    int size;
+    public DynamicArray() {
+        size=0;
+        data = new int[1];
+    }
+    //method get
+    public  int get(int index){
+        return  data[index];
+    }
 }
